c3e9241327f288a0ce74bbf66a52da5e2bc9e40f
daxiazhanshenzhao <153145899+daxiazhanshenzhao@users.noreply.github.com>
2026-01-07 17:21:53 +0800

    提交

--- src/main/java/org/nomoremagicchoices/mixin/MixinA.java
+++
@@ -1,9 +0,0 @@
-package org.nomoremagicchoices.mixin;
-
-
-import net.minecraft.client.Minecraft;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(value = Minecraft.class, priority = 999)
-public class MixinA {
-}
